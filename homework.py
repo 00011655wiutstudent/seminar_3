6926124fb668cd19c05076e30ee59e5635507f6b
00011655wiutstudent <00011655wiutstudent@gmail.com>
2022-11-02 10:07:38 +0500

    new update

---
+++ homework.py
@@ -0,0 +1,22 @@
+#task 1
+#here name and surname are parementers
+def yourname(name, surname):
+    print("Your full name is {} {}".format(name, surname))
+
+
+name = input("What is your name ? ")
+surname = input("What is your surname ? ")
+
+yourname(name, surname)
+#here function takes name and surname as argument
+
+
+#task 2
+def example_1():
+    age =int(input("How old are you ? "))
+
+
+
+
+
+
